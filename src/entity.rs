d5560ac77efeced510226c5e41afb8d06ef00e36
matt-36 <74497645+matt-36@users.noreply.github.com>
2021-11-07 01:42:12 +0000

    updates

--- src/entity.rs
+++ src/entity.rs
@@ -1,77 +1,70 @@
-use std::clone;
+use sdl2::{keyboard::Keycode, rect::{Point, Rect}, render::{Texture, WindowCanvas}};
 
-use sdl2::{render::{Texture}};
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Movement(pub bool, pub bool, pub bool, pub bool);
 
-#[derive(Copy, Clone)]
-pub enum EntityTypes {
-    PlayerType,
-    MobType,
-    CollisionType
+impl Movement {
+    pub fn get_speed(&self)->(i32, i32){
+        ((self.1 as i32) - (self.3 as i32), (self.0 as i32) - (self.2 as i32))
+    }
+    pub fn should_play_animation(&self) -> bool {
+        (self.0 ^ self.2) | (self.1 ^ self.3)
+    }
+    pub fn set_by_key(&mut self, keycode: Keycode, up_or_down: bool){
+        match keycode {
+            Keycode::W => self.2 = up_or_down,
+            Keycode::A => self.3 = up_or_down,
+            Keycode::S => self.0 = up_or_down,
+            Keycode::D => self.1 = up_or_down,
+            _ => {}
+        }
+    }
 }
 
-#[derive(Copy, Clone)]
-pub enum State {
-    Alive,
-    Dead,
-    Respawning,
+
+#[derive(Debug, Clone, Copy)]
+pub struct Player {
+    pub sprite: Rect,
+    pub dest: Rect,
+    pub movement: Movement,
+    pub flip: bool,
 }
 
-
-pub struct Entity {
-    texture: (),
-    pub p_x: i32,
-    pub p_y: i32,
-    pub s_x: i32,
-    pub s_y: i32,
-    pub type_: EntityTypes,
-    pub health: i32,
-    pub state: State
-}
-
-impl Entity {
-    // create a new entity
+impl Player {
+    /// initialize a player
     pub fn new(
-        texture: &Texture, 
-        x_coordinate: i32, 
-        y_coordinate: i32,
-        x_scale: i32, 
-        y_scale: i32, 
-        type_: EntityTypes
-    ) -> Entity {
-        Entity {
-            texture,
-            p_x: x_coordinate,
-            p_y: y_coordinate,
-            s_x: x_scale,
-            s_y: y_scale,
-            type_,
-            health: 100,
-            state: State::Alive
+        sprite: Rect,
+    ) -> Self {
+        let mut dest = Rect::new(0, 0, 32 * 4, 32 * 4);
+        dest.center_on(Point::new(60 as i32 / 2,60 as i32 / 2));
+        Self {
+            sprite,
+            dest,
+            movement: Movement::default(),
+            flip: false
         }
     }
 
-    pub fn attack(&self, who: Entity) {
-        'attacking: loop {
-            
-        }
+    pub fn render(&mut self, canvas: &mut WindowCanvas, texture: &Texture) {
+        canvas.clear();
+
+
+        
+        
+        self.flip = match (self.movement.1 as i8) - (self.movement.3 as i8) {
+            -1 => true,
+            1 => false,
+            _ => self.flip
+        };
+
+        canvas.copy_ex(
+            texture, 
+            Some(self.sprite), 
+            Some(self.dest), 
+            0.0, 
+            None, 
+            self.flip, 
+            false
+        ).expect("failed to render player");
     }
-
-    pub fn move_to(mut self, x_coordinate: i32, y_coordinate: i32) {
-        self.p_x = x_coordinate;
-        self.p_y = y_coordinate;
-    }
-
-    /// Get a reference to the entity's texture.
-    pub fn get_texture(&self) -> &Texture {
-        &self.texture
-    }
-}
-
-
-trait Player {
-    
-}
-
-impl Player for Entity<'_> {
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 extern crate sdl2;
-extern crate serde_json;
 extern crate serde;
+extern crate serde_json;
 
 mod animation;
 mod entity;
-mod game;
 mod events;
+mod game;
 use std::path::Path;
-use std::time::Duration;
 
-use entity::Entity;
-use sdl2::event::Event;
+use sdl2::{event::Event, video::FullscreenType};
+// use sdl2::gfx::framerate::FPSManager;
+use sdl2::image::InitFlag;
 use sdl2::keyboard::Keycode;
-use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
-use sdl2::render::{Canvas, Texture, TextureCreator};
-use sdl2::video::{Window, WindowContext};
-use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::rect::Rect;
+use sdl2::render::TextureCreator;
+use std::time::{Duration, SystemTime};
 
+const GAME_SIZE_X: u32 = 768;
+const GAME_SIZE_Y: u32 = 432;
 
 fn something(ticks: u32, fpm: u32) -> i32 {
     32 * ((ticks / 100) % fpm) as i32
 }
 
-
 fn main() -> Result<(), String> {
+    let mut MOVEMENT_SPEED = 4;
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
@@ -33,13 +33,10 @@ fn main() -> Result<(), String> {
     // however you can only manipulate properties of that window, like its size, whether it's
     // fullscreen, ... but you cannot change its content without using a Canvas or using the
     // `surface()` method.
-    let window = video_subsystem
-        .window(
-            "mountain of doom",
-            800,
-            600,
-        )
+    let mut window = video_subsystem
+        .window("rust game", GAME_SIZE_X, GAME_SIZE_Y)
         .position_centered()
+        .allow_highdpi()
         .build()
         .map_err(|e| e.to_string())?;
 
@@ -51,7 +48,7 @@ fn main() -> Result<(), String> {
         .present_vsync()
         .build()
         .map_err(|e| e.to_string())?;
-
+    canvas.set_integer_scale(true);
     println!("Using SDL_Renderer \"{}\"", canvas.info().name);
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     // clears the canvas with the color we set in `set_draw_color`.
@@ -69,69 +66,91 @@ fn main() -> Result<(), String> {
         .create_texture_from_surface(&temp_surface)
         .map_err(|e| e.to_string())?;
     let timer = sdl_context.timer()?;
-    let mut player = Rect::new(0, 0, 32, 32);
-    let mut dest_player = Rect::new(0, 0, 32 * 4, 32 * 4);
-    dest_player.center_on(Point::new(60, 60));
+    let mut player = entity::Player::new(Rect::new(0, 0, 32, 32));
 
+    // let fpsmanager = FPSManager::new();
+    let mut fullscreen = false;
     let mut event_pump = sdl_context.event_pump()?;
-    let mut flip = false;
+    let mut frame_times = [0u128; 60];
+    let mut frames = 0;
+    let mut last_frame_time = SystemTime::now();
     'running: loop {
         // get the inputs here
         for event in event_pump.poll_iter() {
+            if let Event::KeyDown { repeat: false, .. } | Event::KeyUp { .. } = event {
+                println!("recieved {:?}", event);
+            };
+
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
-                    keycode: Some(Keycode::End),
-                    ..
-                } => break 'running,
-                Event::KeyDown {
-                    keycode: Some(Keycode::W),
+                    keycode: Some(Keycode::Escape),
                     ..
                 } => {
-                    player.x = something(timer.ticks(), 4u32);
-                    dest_player.y-=4;
-                },
+                    break 'running;
+                }
                 Event::KeyDown {
-                    keycode: Some(Keycode::A),
+                    keycode: Some(Keycode::F11),
                     ..
                 } => {
-                    flip = true;
-                    player.x = something(timer.ticks(), 4u32);
-                    dest_player.x-=4;
-                },
+                    // canvas.window_mut().set_size(1920, 1080).expect("fuck");
+                    let window = canvas.window_mut();
+                    window
+                        .set_fullscreen(if fullscreen {
+                            FullscreenType::Desktop
+                        } else {
+                            FullscreenType::Off
+                        })
+                        .expect("fuck");
+                    let size = window.size();
+                    canvas
+                        .set_scale(
+                            (size.0 as f32) / (GAME_SIZE_X as f32),
+                            (size.1 as f32) / (GAME_SIZE_Y as f32),
+                        )
+                        .expect("fuck");
+                    fullscreen = !fullscreen;
+                }
+
                 Event::KeyDown {
-                    keycode: Some(Keycode::S),
+                    keycode: Some(keycode),
                     ..
                 } => {
-                    player.x = something(timer.ticks(), 4u32);
-                    dest_player.y+=4;
-                },
-                Event::KeyDown {
-                    keycode: Some(Keycode::D),
+                    player.movement.set_by_key(keycode, true);
+                }
+                Event::KeyUp {
+                    keycode: Some(keycode),
                     ..
                 } => {
-                    flip = false;
-                    player.x = something(timer.ticks(), 4u32);
-                    dest_player.x+=4;
-                },
+                    player.movement.set_by_key(keycode, false);
+                }
                 _ => {}
             }
         }
 
         // update the game loop here
+        if player.movement.should_play_animation() {
+            player.sprite.x = something(timer.ticks(), 4);
+        }
+        let player_speed = player.movement.get_speed();
+        player.dest.x += player_speed.0 * MOVEMENT_SPEED;
+        player.dest.y += player_speed.1 * MOVEMENT_SPEED;
 
-
-        canvas.clear();
-        canvas.copy_ex(
-            &texture,
-            Some(player),
-            Some(dest_player),
-            0.0,
-            None,
-            flip,
-            false,
-        )?;
+        player.render(&mut canvas, &texture);
         canvas.present();
+        let frame_time = SystemTime::now();
+        frame_times[frames % 60] = frame_time
+            .duration_since(last_frame_time)
+            .unwrap()
+            .as_nanos();
+        frames += 1;
+        last_frame_time = frame_time;
+        // ((frame_times.into_iter().map(|v|frame_time - v).sum::<u128>()) * 1_000_000_000);
+        // println!("{:?}", 60f64 / (frame_times.into_iter().map(|v|frame_time - v).sum::<u128>() as f64) * 1_000_000_000f64);
+        println!(
+            "{:?}",
+            60f64 / frame_times.iter().sum::<u128>() as f64 * 1_000_000_000f64
+        );
         std::thread::sleep(Duration::from_millis(0));
     }
     Ok(())
